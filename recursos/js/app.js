2afdd91c3a379015a2941689002646529b984920
rodfaro <rodrigofaro2005@gmail.com>
2026-04-30 14:10:37 -0300

    Correcciones index y app.js

--- recursos/js/app.js
+++ recursos/js/app.js
@@ -1,18 +1,32 @@
 import { renderizarProductos } from "./render.js";
 import { obtenerAlfajores } from "./api.js";
+import { renderizarProductosIndex } from "./render.js";
 
 const url = '/recursos/data/alfajores.json'; //ruta para el fetch
 
 
 //index.html
 
+const $catalogoIndexMain = document.getElementById('catalogo-index-main'); 
 const $catalogoIndex = document.getElementById('catalogo-index'); //Contiene el elemento/caja del catalogo del html
 
 //obtener datos desde una api/json
+
 const alfajores = await obtenerAlfajores(url)
 
-//funciones para renderizar
-renderizarProductos(alfajores, $catalogoIndex)
+
+
+if ($catalogoIndex) {
+  // Página productos → mostrar TODOS
+  renderizarProductos(alfajores, $catalogoIndex);
+}
+
+if ($catalogoIndexMain != null) {
+  // Página productos → mostrar TODOS
+  const alfajoresIndex = alfajores.slice(0,3)
+  renderizarProductos(alfajoresIndex, $catalogoIndexMain);
+}
+
 
 //funciones botones del index
 const $botones = document.querySelectorAll('.botonCatalogo')
--- recursos/js/render.js
+++ recursos/js/render.js
@@ -18,4 +18,25 @@ function renderizarProductos(productos, $elementoDOM){
     $elementoDOM.innerHTML = html
 }
 
+function renderizarProductosIndex(productos, $elementoDOM){
+    
+    let html = ''
+    
+    productos.forEach((alfajor) => {
+        const plantilla = `
+            <div class="carta-producto">
+                <img src="${alfajor.img}" alt="alfajor clasico con DDL">
+                <h3>${alfajor.nombre}</h3>
+                <p>$${alfajor.precio}</p>
+                <button data-id = "${alfajor.id}" class="botonCatalogo">Añadir al carrito</button>
+            </div>
+        ` 
+
+        html += plantilla
+    });
+
+    $elementoDOM.innerHTML = html
+}
+
+export {renderizarProductosIndex}
 export {renderizarProductos}
